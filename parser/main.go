de2f13b6da9c59289b3b50f4c2b91e9594f3aa1e
user <lyovkin@i.ua>
2018-10-24 19:01:26 +0300

    rid of concurrency

--- parser/main.go
+++ parser/main.go
@@ -17,42 +17,6 @@ const (
 	link = "centre:8080"
 )
 
-//func processLine(line []string, conn *grpc.ClientConn, wg sync.WaitGroup) {
-//	log.Print("processLine")
-//
-//	c := api.NewDataFlowClient(conn)
-//
-//	response, err := c.SendData(context.Background(), &api.DataMessage{Name: line[1], Email: line[2], Mobile: line[3]})
-//	log.Print("we have response")
-//
-//	if err != nil {
-//		log.Fatalf("Error when calling SendData: %s", err)
-//	}
-//	log.Printf("%t", response.Ok)
-//
-//	wg.Done()
-//}
-//
-//
-//func process(reader *csv.Reader, conn *grpc.ClientConn) {
-//	var wg sync.WaitGroup
-//
-//	for i := 1 ; ; i++ {  // i = 1 - skip header
-//		line, e := reader.Read()
-//		if e == io.EOF {
-//			return
-//		} else if e != nil {
-//			log.Fatal(e)
-//		}
-//
-//		wg.Add(1)
-//		go processLine(line, conn, wg)
-//	}
-//
-//
-//	wg.Wait()
-//}
-
 func main() {
 	var conn *grpc.ClientConn
 
@@ -71,8 +35,6 @@ func main() {
 
 	csvFile, _ := os.Open("./temp/data/data.csv")
 	reader := csv.NewReader(bufio.NewReader(csvFile))
-
-	//process(reader, conn)
 
 	for i := 1; ; i++ { // skip header
 		line, e := reader.Read()
